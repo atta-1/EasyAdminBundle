1871ea0856b63478eb801cee9cd7bd7a1da6aaf9
Alex Niedre <alex.niedre@atta.lv>
2025-12-02 12:07:18 +0100

    WT-131: Sync EasyAdminBundle with our fork

--- src/Contracts/Factory/FieldFactoryInterface.php
+++ src/Contracts/Factory/FieldFactoryInterface.php
@@ -2,10 +2,13 @@
 
 namespace EasyCorp\Bundle\EasyAdminBundle\Contracts\Factory;
 
+use EasyCorp\Bundle\EasyAdminBundle\Collection\EntityCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 
 interface FieldFactoryInterface
 {
     public function processFields(EntityDto $entityDto, FieldCollection $fields): void;
+
+    public function processFieldsForAll(EntityCollection $entityDtos, FieldCollection $fields, ?string $currentPage = null): void;
 }
